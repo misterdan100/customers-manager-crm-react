aac79db2b5e62cee96ef480c5e37ddbda305c72e
misterdan100 <danielmca60@gmail.com>
2023-11-24 06:31:31 -0500

    Function delete customer, w/ delete api request

--- src/components/Cliente.jsx
+++ src/components/Cliente.jsx
@@ -1,8 +1,14 @@
-import { Navigate, useNavigate } from "react-router-dom";
+import { useNavigate, Form, redirect } from "react-router-dom"
+import { eliminarCliente } from "../data/clientes";
 
-const Cliente = ({cliente}) => {
-  const navigate = useNavigate()
-    const { nombre, empresa, email, telefono, id } = cliente
+export async function action({ params }) {
+  await eliminarCliente(params.clienteId)
+  return redirect('/')
+}
+
+const Cliente = ({ cliente }) => {
+  const navigate = useNavigate();
+  const { nombre, empresa, email, telefono, id } = cliente;
   return (
     <tr className="border-b">
       <td className="p-6 space-y-2">
@@ -11,24 +17,42 @@ const Cliente = ({cliente}) => {
       </td>
 
       <td className="p-6">
-        <p className="text-gray-600"><span className="text-gray-800 uppercase font-bold">Email: </span>{email}</p>
-        <p className="text-gray-600"><span className="text-gray-800 uppercase font-bold">Telefono: </span>{telefono}</p>
+        <p className="text-gray-600">
+          <span className="text-gray-800 uppercase font-bold">Email: </span>
+          {email}
+        </p>
+        <p className="text-gray-600">
+          <span className="text-gray-800 uppercase font-bold">Telefono: </span>
+          {telefono}
+        </p>
       </td>
 
       <td className="p-6 flex gap-4">
         <button
-            type="button"
-            className="text-blue-600 hover:text-blue-700 uppercase font-bold text-xs"
-            onClick={() => navigate(`/clientes/${id}/editar`)}
+          type="button"
+          className="text-blue-600 hover:text-blue-700 uppercase font-bold text-xs"
+          onClick={() => navigate(`/clientes/${id}/editar`)}
         >
-            Editar
+          Editar
         </button>
-        <button
-            type="button"
+
+        <Form
+          method="POST"
+          action={`/clientes/${id}/eliminar`}
+          onSubmit={(e) => {
+            if(!confirm('Deseas eliminar este registro?')) {
+              //* Default action in this case is call the action function
+              e.preventDefault()
+            }
+          }}
+        >
+          <button
+            type="submit"
             className="text-red-600 hover:text-red-700 uppercase font-bold text-xs"
-        >
+          >
             Eliminar
-        </button>
+          </button>
+        </Form>
       </td>
     </tr>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import NuevoCliente, {action as nuevoClienteAction} from './pages/NuevoCliente'
 import Index, { loader as clientesLoader } from './pages/Index'
 import ErrorPage from './components/ErrorPage'
 import EditarCliente, { loader as editarClienteLoader, action as editarClienteAction } from './pages/EditarCliente'
-
+import { action as eliminarClienteAction } from './components/Cliente'
 import './index.css'
 
 const router = createBrowserRouter([
@@ -33,6 +33,10 @@ const router = createBrowserRouter([
         loader: editarClienteLoader,
         action: editarClienteAction,
         errorElement: <ErrorPage />
+      },
+      {
+        path: '/clientes/:clienteId/eliminar',
+        action: eliminarClienteAction,
       }
     ]
   },
